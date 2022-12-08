c98ae4bc509b571cdddec51dbd72c8a3ef7630cf
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-12-08 16:34:51 +0530

    Refactor:Added User Valid Pattern By Using Regex

--- src/main/java/com/myfirstservlet/LoginServlet.java
+++ src/main/java/com/myfirstservlet/LoginServlet.java
@@ -1,5 +1,6 @@
 package com.myfirstservlet;
 
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -9,32 +10,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet testing",
         urlPatterns = { "/LoginServlet" },
-        initParams = { @WebInitParam(name="user",value="Ashutosh"),
+        initParams = {
+                @WebInitParam(name="user",value="Ashutosh"),
                 @WebInitParam(name="password",value="Pass1234")
         }
 )
 
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
-        String user = req.getParameter("user");
-        String pwd = req.getParameter("pwd");
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+        //get request parameters for userID and password
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        //get servlet config init params
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-
-        if (userID.equals(user) && password.equals(pwd)){
-            req.setAttribute("user",user);
-            req.getRequestDispatcher("LoginSuccess.jsp").forward(req,response);
-        } else {
+        //Check Name Using Regex
+        String nameRegex = "[A-Z]{1}[a-z]{2,}";
+        Pattern npattern = Pattern.compile(nameRegex);
+        Matcher fmatcher = npattern.matcher(user);
+        if(fmatcher.matches()) {
+            if (userID.equals(user) && password.equals(pwd)) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+            } else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+                PrintWriter out = response.getWriter();
+                out.println("<font color=red>Either user name or password is wrong.</font>");
+                rd.include(request, response);
+            }
+        }else{
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.</font>");
-            rd.include(req,response);
+            out.println("<font color=red>Either name or password format is wrong.</font>");
+            rd.include(request, response);
         }
+
     }
 }
